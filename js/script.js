dad9e446ed96d28a85fdf30d8ebeccf647bb03ca
B-Rumbl <78166511+B-Rumbl@users.noreply.github.com>
2022-11-17 14:09:33 +0000

    Update 17/11

--- js/script.js
+++ js/script.js
@@ -2,3 +2,8 @@ document.getElementById("orb009").addEventListener("click", pg2Select);
 function pg2Select(){
     window.location.href = "page2.html"
 }
+
+document.getElementById("orb000").addEventListener("click", nilBunny);
+function nilBunny(){
+    window.location.href = "nilbunny.html"
+}
